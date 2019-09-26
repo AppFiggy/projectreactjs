d882405679e12ae6d3bcaf85b0b78b9f74620831
Newton Soares <newton@figgy.com.br>
2019-09-25 21:58:24 -0300

    feature: list repositories

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/prefer-default-export */
 import styled, { keyframes, css } from 'styled-components';
 
 export const Container = styled.div`
@@ -70,4 +69,27 @@ export const SubmitButton = styled.button.attrs(props => ({
         animation: ${rotate} 2s linear infinite;
       }
     `}
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    /*Aplica uma estilização em todos os itens exceto no primeiro*/
+    & + li {
+      border-top: 1px solid #eee;
+    }
+
+    a {
+      color: #7159c1;
+      text-decoration: none; /*remove o underline*/
+    }
+  }
 `;
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // endereço da api padronizada em todas as chamadas api
-  baseURL: 'http://api.github.com',
+  baseURL: 'https://api.github.com',
 });
 
 export default api;
